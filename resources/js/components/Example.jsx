d4dfc2ebb2e3a01e92a27defe26da58845fdf902
Juan Carlos Charparin <jccharparin@gmail.com>
2024-07-14 20:14:51 -0300

    proyecto english-club

---
+++ resources/js/components/Example.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Example() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">Bienvenidos</div>
+
+                        <div className="card-body">Soy un componente de react</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Example;
+
+
