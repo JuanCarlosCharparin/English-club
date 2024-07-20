bb565e8d966289e55bf7b4bd3793b622ddffa313
Juan Carlos Charparin <jccharparin@gmail.com>
2024-07-19 23:04:02 -0300

    Punto de inicio

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*Route::prefix('v1')->group(function () {
+    // PUBLIC ROUTE
+    Route::get('/post/{slug}', [HomeController::class, 'index']);
+
+    // Auth
+    Route::get('/auth/register', [AuthController::class, 'register']);
+    Route::get('/auth/login', [AuthController::class, 'login']);
+
+    // PRIVATE ROUTE
+    Route::group(['middleware' => 'auth:sanctum'], function(){
+        //auth
+        Route::get('/auth/logout', [AuthController::class, 'logout']);
+
+
+        //rol client
+        Route::apiResource('/client/instructor', [InstructorController::class, 'logout']);
+
+        //rol admin
+        Route::apiResource('/ad/instructor', [InstructorController::class, 'logout']);
+    });
+    // Route::get('/post/{slug}', [CursoController::class, 'index']);
+});*/
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
